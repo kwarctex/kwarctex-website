9c5e7f92a294bd227486e588813d5a22fddb43cd
kwarctex <biuro.kwarctex@gmail.com>
2019-12-23 17:16:10 +0100

    add resons for kwarctex template

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import MainTemplate from 'templates/MainTemplate/MainTemplate/';
 import HeroTemplate from 'templates/HeroTemplate/HeroTemplate/';
 import AboutTemplate from 'templates/AboutTemplate/AboutTemplate/';
+import ResonsForKwarctexTemplate from 'templates/ResonsForKwarctexTemplate/ResonsForKwarctexTemplate/';
 
 const IndexPage = () => (
   <div>
     <MainTemplate>
       <HeroTemplate />
       <AboutTemplate />
+      <ResonsForKwarctexTemplate />
     </MainTemplate>
   </div>
 );
---
+++ src/templates/ResonsForKwarctexTemplate/ResonsForKwarctexTemplate.js
@@ -0,0 +1,6 @@
+import React from 'react';
+// import styled from 'styled-components';
+
+const ResonsForKwarctexTemplate = () => <h2>hello</h2>;
+
+export default ResonsForKwarctexTemplate;
