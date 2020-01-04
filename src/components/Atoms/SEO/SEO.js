4622ccdfac922e9ab86ce612e5916fe95835f69d
kwarctex <biuro.kwarctex@gmail.com>
2020-01-04 18:13:50 +0100

    fixes

--- src/components/Atoms/SEO/SEO.js
+++ src/components/Atoms/SEO/SEO.js
@@ -5,6 +5,10 @@ const SEO = () => (
   <Helmet>
     <html lang="pl" />
     <title>Kwarctex - konglomeraty kwarcowe</title>
+    <meta
+      name="format-detection"
+      content="telephone=no,date=no,address=no,email=no,url=no"
+    />
     <link
       href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600&display=swap"
       rel="stylesheet"
